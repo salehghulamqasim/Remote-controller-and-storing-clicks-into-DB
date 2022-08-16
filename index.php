e910fd0bcd7a5aafa4bba791ed927f7284652175
Saleh <100453330+salehghulamqasim@users.noreply.github.com>
2022-08-16 20:17:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
+<?php
+$connection = mysqli_connect("localhost",'root','','clickData');
 
+if(isset($_GET['up']))
+{
+	echo "up button was clicked";
+	$mysql = "INSERT INTO `clicks`(clicks) VALUES('up')";
+	$connection->query($mysql);
+} else if(isset($_GET['down']))
+{
+	echo "down button was clicked";
+	$mysql = "INSERT INTO clicks(clicks) VALUES('down')";
+	$connection->query($mysql);
+} else if(isset($_GET['right']))
+{
+	echo "Right button was clicked";
+	$mysql = "INSERT INTO clicks(clicks) VALUES('right')";
+	$connection->query($mysql);
+}else if(isset($_GET['left']))
+{
+	echo "Left button was clicked";
+	$mysql = "INSERT INTO clicks(clicks) VALUES('left')";
+	$connection->query($mysql);
+}else if(isset($_GET['stop']))
+{
+	echo "Stopped";
+	$mysql = "INSERT INTO clicks(clicks) VALUES('stop')";
+	$connection->query($mysql);
+}
+?>
+
+  </html>
 <!DOCTYPE html>
 <html>
 
@@ -8,6 +39,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controller</title>
     <link rel="stylesheet" href="style.css">
+    <script type='text/php' src="db.php"></script>
 
    
 </head>
@@ -20,32 +52,26 @@
             <br>
             <img src="https://cdn-icons-png.flaticon.com/512/1451/1451405.png" alt="alternatetext"  style="width:50px;height:50px;">
         </legend>
-        <form action="db.php" method="post">
+        <form action="index.php" method="get">
           <div id="container">
             <nav>
-                <button class="button top"   id="up"     type="submit"></button> 
-                <button class="button right" id="right"  type="submit"> </button> 
-                <button class="button left"  id="left"   type="submit"></button> 
-                <button class="button bottom"id="down"   type="submit"></button> 
-                <button class="center-button"id="stop"   type="submit"></button>
-                <button id="voice">click here</button>
-
+                <button  name="up" class="button top"      id="up"     type="submit"></button> 
+                <button  name="right" class="button right" id="right"  type="submit"> </button> 
+                <button  name="left" class="button left"   id="left"   type="submit"></button> 
+                <button  name="down" class="button bottom" id="down"   type="submit"></button> 
+                <button  name="stop" class="center-button" id="stop"   type="submit"></button>
               </nav>
               <br>
-              
               <p>for voice control</p>
-              
+              <button id="voice">click here</button>
               </div>
 
 
         </form>
-             
     </center>
 
 </body>
 
 </html>
 
-<!-- ToDo
-        1-add text or arrow inside buttons
-        2-connect them with the php -->
+
